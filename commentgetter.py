6fcde6ab7899fd879537a40c115d8581d49fe448
Noodle <18282639953@163.com>
2025-07-10 09:49:19 +0800

    version1.2 25/7/10

--- commentgetter.py
+++ commentgetter.py
@@ -16,24 +16,38 @@ def fetch_comments(video_name: str, video_id: str, headers: dict, max_pages: int
     comments = []
     seen_ids = set()
     next_val = 1
+
     for page in range(1, max_pages + 1):
-        url = f'https://api.bilibili.com/x/v2/reply/main?next={next_val}&type=1&oid={video_id}&mode=3'
+        url = (f'https://api.bilibili.com/x/v2/reply/'
+               f'main?next={next_val}&type=1&oid={video_id}&mode=3')
         try:
-            response = requests.get(url, headers=headers, timeout=10)
+
+            response = requests.get(
+                url = url,
+                headers = headers,
+                timeout = 10
+            )
+
             if response.status_code == 200:
                 data = response.json()
+
                 # save data for ananlyze:
                 # with open(f'data/{video_id}_page_{page}.json', 'w', encoding='utf-8') as f:
                 #     f.write(response.text)
                 # breakpoint()
+
                 print(f"第{page}页")
+
                 if not data['data'] or data['data']['replies'] is None:
                     break
+
                 for comment in data['data']['replies']:
                     comment_id = comment['rpid']
                     if comment_id in seen_ids:
                         continue
+
                     seen_ids.add(comment_id)
+
                     comment_info = {
                         '视频名': video_name,
                         '视频id': video_id,
@@ -45,16 +59,23 @@ def fetch_comments(video_name: str, video_id: str, headers: dict, max_pages: int
                         '点赞数量': comment['like'],
                         '回复时间': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(comment['ctime']))
                     }
+
                     comments.append(comment_info)
-                # 翻页
+
+                # next page
                 next_val = data['data']['cursor']['next']
+
                 if not data['data']['replies'] or not next_val:
                     break
+
             else:
                 print(f"请求失败，状态码: {response.status_code}")
                 break
+
         except requests.RequestException as e:
             print(f"请求出错: {e}")
             break
+
         time.sleep(0.75)
+
     return comments
